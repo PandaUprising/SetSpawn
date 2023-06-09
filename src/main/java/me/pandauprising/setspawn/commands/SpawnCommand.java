c78c1dad7bfe994c28d4db93ca5afdf391f0513b
Isaac Brawls <brawlsisaac@gmail.com>
2023-06-09 12:37:42 +0800

    1.20 support

--- src/main/java/me/pandauprising/setspawn/commands/SpawnCommand.java
+++ src/main/java/me/pandauprising/setspawn/commands/SpawnCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -21,7 +22,7 @@ public class SpawnCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "Only players can use this command!");
